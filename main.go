ac3855f6657f5b4b120946709d01caad890bbb83
Billy <bbray@cloudentity.com>
2022-01-21 15:25:39 -0500

    test

--- main.go
+++ main.go
@@ -15,16 +15,15 @@ import (
 )
 
 var (
-	acpOAuthConfig acp.Config
-	clientID       = flag.String("clientId", "", "Application client ID")
-	issuerURL      = flag.String("issuerUrl", "https://localhost:8443/default/default", "Issuer URL with provided tenant, and server ID")
-	port           = flag.String("port", "18888", "Port where callback, and login endpoints will be exposed")
-	host           = flag.String("host", "localhost", "Host where your client applications is running")
-	redirectHost   = flag.String("redirectHost", "localhost", "Host where the OAuth Server will redirect to")
-	certPath       = flag.String("cert", "certs/acp_cert.pem", "A path to the file with a certificate")
-	keyPath        = flag.String("key", "certs/acp_key.pem", "A path to the file with a private key")
-	rootCA         = flag.String("serverCert", "certs/ca.pem", "A path to the file with rootCA")
-	pkceEnabled    = flag.Bool("pkce", false, "Enables PKCE flow")
+	clientID     = flag.String("clientId", "", "Application client ID")
+	wellKnown    = flag.String("wellKnown", "https://cloudentity-bbray.mtls.us.authz.cloudentity.io/cloudentity-bbray/default/.well-known/openid-configuration", "Issuer URL with provided tenant, and server ID")
+	port         = flag.String("port", "18888", "Port where callback, and login endpoints will be exposed")
+	host         = flag.String("host", "localhost", "Host where your client applications is running")
+	redirectHost = flag.String("redirectHost", "localhost", "Host where the OAuth Server will redirect to")
+	certPath     = flag.String("cert", "certs/acp_cert.pem", "A path to the file with a certificate")
+	keyPath      = flag.String("key", "certs/acp_key.pem", "A path to the file with a private key")
+	rootCA       = flag.String("serverCert", "certs/ca.pem", "A path to the file with rootCA")
+	pkceEnabled  = flag.Bool("pkce", false, "Enables PKCE flow")
 
 	secureCookie = securecookie.New(securecookie.GenerateRandomKey(64), securecookie.GenerateRandomKey(32))
 )
@@ -42,6 +41,12 @@ func main() {
 		client      acp.Client
 		err         error
 	)
+
+
+	testClient, err := http.Get(*wellKnown)
+
+	
+
 
 	flag.Parse()
 
@@ -61,10 +66,13 @@ func main() {
 		log.Fatalln("a client ID is required")
 	}
 
+	token, _ := url.Parse("https://cloudentity-bbray.mtls.us.authz.cloudentity.io/cloudentity-bbray/default/oauth2/token")
+
 	cfg := acp.Config{
 		ClientID:    *clientID,
 		RedirectURL: redirectURL,
 		IssuerURL:   url,
+		TokenURL:    token,
 		CertFile:    *certPath,
 		KeyFile:     *keyPath,
 		RootCA:      *rootCA,
